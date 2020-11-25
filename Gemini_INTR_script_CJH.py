8e782b46ce2435a995a89ef96343cb4c851f7233
charleshages <65269370+charleshages@users.noreply.github.com>
2020-11-24 22:50:31 -0500

    Add files via upload

--- Gemini_INTR_script_CJH.py
+++ Gemini_INTR_script_CJH.py
@@ -5,28 +5,56 @@ Created on Fri Oct 23 12:20:31 2020
 @author: Chuck
 """
 
-from interferogram_functions import prep_interferogram, import_INTR, FFT_intr
+from interferogram_functions import prep_interferogram, import_INTR, FFT_intr, import_MAP
 import matplotlib.pyplot as plt
+import os
+import numpy as np
 
-path = r"C:\Users\Chuck\Dropbox (UFL)\Hages Lab Files\TRPL Data\Ruiquan\20201104\153345"
+#Apply to "*_INTR.txt" data
+path = r"\\ad.ufl.edu\che\Hages-Lab\TRPL Data\Hages\Ferekides\20_11_24\152430"
 pos_data, intr_data = import_INTR(path)
-      
-apodization_width=0.5
+
+apodization_width=1.75
 apod_type="BH"    # "None" "Gauss" "Triangle" "Boxcar" "BH"
 resample="True"
-resample_factor=2
+resample_factor=4
 shift="True"
 pad_test="True"
 padfactor=4
-mean_sub = "True"       
-   
+mean_sub = "True"
+
 preFFT_pos, preFFT_data, shiftfactor = prep_interferogram(pos_data,intr_data,apodization_width,apod_type=apod_type,resample=resample,resample_factor=resample_factor,shift=shift,pad_test=pad_test,padfactor=padfactor,mean_sub=mean_sub,plots="True")
 wave, FFT_intr_trim = FFT_intr(preFFT_pos,preFFT_data,plots="True",scale="linear",correct="True")
 
-start_wave = 400
+start_wave = 700
 end_wave = 1000
 plt.figure(3, dpi=120)
 plt.plot(wave,FFT_intr_trim)
 plt.xlim(start_wave,end_wave)
 plt.yscale('linear')
 plt.show()
+
+
+
+# =============================================================================
+# #Apply to single line of MAP data
+# pos_data, time_data, map_data = import_MAP(path)
+# pos_data = pos_data - shiftfactor    #Taken from shift_factor output in the _INTR analysis script for this data
+#
+# #ADD MANUAL SHIFT by shift factor - currently done above manually
+# #WHAT ABOUT INTENSITY CORRECTION? Ask NIREOS
+# shift="False"
+#
+# #pick time to check
+# rangeval = 12  #ns
+# index = (np.abs(time_data-np.min(rangeval))).argmin()
+#
+# preFFT_pos, preFFT_data, shiftfactor = prep_interferogram(pos_data,map_data[:,index],apodization_width,apod_type=apod_type,resample=resample,resample_factor=resample_factor,shift=shift,pad_test=pad_test,padfactor=padfactor,mean_sub=mean_sub,plots="True")
+# wave, FFT_intr_trim = FFT_intr(preFFT_pos,preFFT_data,plots="True",scale="linear",correct="True")
+#
+# plt.figure(4, dpi=120)
+# plt.plot(wave,FFT_intr_trim)
+# plt.xlim(start_wave,end_wave)
+# plt.yscale('linear')
+# plt.show()
+# =============================================================================
--- Gemini_MAP_script_CJH.py
+++ Gemini_MAP_script_CJH.py
@@ -4,69 +4,80 @@ Created on Sat Oct 24 23:08:58 2020
 
 @author: Chuck
 """
-from interferogram_functions import FFT_intr, import_MAP, prep_map
+from interferogram_functions import FFT_map, import_MAP, prep_map
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+import h5py
+import os
 
-path = r"C:\Users\Chuck\Dropbox (UFL)\Hages Lab Files\TRPL Data\Ruiquan\20201104\153345"
+startTime = time.time()
+path = r"C:\Users\Chuck\Dropbox (UFL)\UF\TRPL Computer\152430"
+save_data = True
+outputfilename = path + "\\" + os.path.split(path)[-1] + '_TRES.h5'
 pos_data, time_data, map_data = import_MAP(path)
-pos_data = pos_data - 0.03478240614951511    #Taken from shift_factor output in the _INTR analysis script for this data
 
-apodization_width=0.6
+#trim time-scale
+rangeval = [0,100]  #ns
+index = [(np.abs(time_data-np.min(rangeval))).argmin(),(np.abs(time_data-np.max(rangeval))).argmin()]
+map_data = map_data[:,np.min(index):np.max(index)]
+time_data=time_data[np.min(index):np.max(index)]
+
+#Manual Shift of Peak
+pos_data = pos_data - 0.15619633484742135    #Taken from shift_factor output in the _INTR analysis script for this data
+
+#Background Subtract TRPL Curves
+BKGsub = "True"
+BKGrange = [0,5]  #ns
+
+if BKGsub == "True":
+    index = [(np.abs(time_data-np.min(BKGrange))).argmin(),(np.abs(time_data-np.max(BKGrange))).argmin()]
+    BKGval = np.mean(map_data[:,np.min(index):np.max(index)],axis=1)
+    for i in range(len(BKGval)):
+        map_data[i,:] = map_data[i,:]-BKGval[i]
+
+apodization_width=1.75
 apod_type="BH"    # "None" "Gauss" "Triangle" "Boxcar" "BH"
 resample="True"
 resample_factor=4
+shift="True"
 pad_test="True"
 padfactor=4
-mean_sub = "True"         
+mean_sub = "True"
 
-#Plot Raw Data
-raw_timemesh, raw_posmesh = np.meshgrid(time_data,pos_data)
-plt.figure(1, dpi=120)
-plt.contourf(raw_posmesh,raw_timemesh,np.log(map_data))
-plt.ylim(5,25)
-plt.ylabel('Time / ns')
-plt.xlabel('Position / mm')
+# =============================================================================
+# #Plot Raw Data (Background Subtracted and Shifted)
+# raw_timemesh, raw_posmesh = np.meshgrid(time_data,pos_data)
+# plt.figure(1, dpi=120)
+# plt.contourf(raw_posmesh,raw_timemesh,np.log(map_data))
+# plt.ylim(5,25)
+# plt.ylabel('Time / ns')
+# plt.xlabel('Position / mm')
+# =============================================================================
 
 preFFT_pos, preFFT_map = prep_map(pos_data,map_data,apodization_width,apod_type=apod_type,resample=resample,resample_factor=resample_factor,shift="False",pad_test=pad_test,padfactor=padfactor,mean_sub=mean_sub,plots="True")
-
-#trim time-scale data because it is dreadfully slow
-rangeval = [5,25]  #ns
-index = [(np.abs(time_data-np.min(rangeval))).argmin(),(np.abs(time_data-np.max(rangeval))).argmin()]
-preFFT_map = preFFT_map[:,np.min(index):np.max(index)]
-time_data=time_data[np.min(index):np.max(index)]
+print("Took {} sec".format(time.time() - startTime))
 
 #Perform FFT
-build_TRES=[]
-for i in range(preFFT_map.shape[1]):
-    wave, FFT_intr_trim = FFT_intr(preFFT_pos,preFFT_map[:,i],plots="False",scale="linear",correct="True")
-    build_TRES.append(FFT_intr_trim)
-build_TRES=np.array(build_TRES,dtype="float").T
-
-#Background Subtract
-BKGsub = "False"
-BKGrange = [5,10]  #ns
-
-if BKGsub == "True":
-    index = [(np.abs(time_data-np.min(rangeval))).argmin(),(np.abs(time_data-np.max(rangeval))).argmin()]
-    BKGval = np.mean(build_TRES[:,np.min(index):np.max(index)])
-    build_TRES = build_TRES-BKGval
+wave, build_TRES = FFT_map(preFFT_pos, preFFT_map,plots="False",scale="linear",correct="True")
+build_TRES=np.array(build_TRES,dtype="float")
+print("Took {} sec".format(time.time() - startTime))
 
 # Plot the results
 timemesh, wavemesh = np.meshgrid(time_data,wave)
-start_wave = 400
+start_wave = 700
 end_wave = 1000
 
 plt.figure(2, dpi=120)
-plt.contourf(wavemesh,timemesh,build_TRES,100,vmin=0)
+plt.contourf(wavemesh,timemesh,np.log(build_TRES))
 plt.xlim(start_wave,end_wave)
 plt.ylabel('Time / ns')
 plt.xlabel('Wavelength / nm')
 plt.colorbar()
 
 #Plot averged PL over given range
-AveragePL = "True"
-rangeval = [7.5,25]  #ns
+AveragePL = "False"
+rangeval = [0,100]  #ns
 
 plt.figure(3, dpi=120)
 if AveragePL == "True":
@@ -80,8 +91,8 @@ plt.xlabel('Wavelength / nm')
 plt.yscale('linear')
 
 #Plot averged PL decay over given range
-AverageTRPL = "True"
-rangeval = [610,750]  #nm
+AverageTRPL = "False"
+rangeval = [800,900]  #nm
 
 plt.figure(4, dpi=120)
 if AverageTRPL == "True":
@@ -93,4 +104,20 @@ plt.xlabel('Time / ns')
 plt.ylabel('Counts / a.u.')
 plt.yscale('log')
 
-#Make composite Plot??
+#Write 2D Data Set
+if save_data:
+    hf = h5py.File(outputfilename,'w')
+    hf.create_dataset('TRES Data', data=build_TRES)
+    hf.create_dataset('Time Data', data=time_data)
+    hf.create_dataset('Wavelength', data=wave)
+    hf.create_dataset('Metadata', data=[[start_wave,end_wave]])
+    hf.close()
+
+# =============================================================================
+# #Make composite Plot??
+# fig = plt.figure(figsize=(6, 6))
+# grid = plt.GridSpec(4, 4, hspace=0.2, wspace=0.2)
+# main_ax = fig.add_subplot(grid[:-1, 1:])
+# y_hist = fig.add_subplot(grid[:-1, 0], xticklabels=[], sharey=main_ax)
+# x_hist = fig.add_subplot(grid[-1, 1:], yticklabels=[], sharex=main_ax)
+# =============================================================================
