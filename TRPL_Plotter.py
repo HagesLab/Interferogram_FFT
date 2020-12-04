30c03f2513503717443cc48da800276053c25e94
charleshages <65269370+charleshages@users.noreply.github.com>
2020-12-04 17:43:35 -0500

    Added fitting functions

--- TRPL_Plotter.py
+++ TRPL_Plotter.py
@@ -9,25 +9,15 @@ Created on Fri Dec  4 14:53:37 2020
 import numpy as np
 import csv
 import matplotlib.pylab as plt
+from scipy.optimize import curve_fit
 
-Plot_Title = r'CdTe 9-8 @ 750 nm'
+Plot_Title = r'CdTe @ 750 nm'
 
-path1 = r"\\ad.ufl.edu\che\Hages-Lab\TRPL Data\Hages\Ferekides\20_12_1\161903\161903_AverageTRPL_800_to_875nm.csv"
-tag1 = r'9-8-1-A'
-
-path2 = r"\\ad.ufl.edu\che\Hages-Lab\TRPL Data\Hages\Ferekides\20_12_1\155433\155433_AverageTRPL_800_to_875nm.csv"
-tag2 = r'9-8-1-B'
-
-path3 = r"\\ad.ufl.edu\che\Hages-Lab\TRPL Data\Hages\Ferekides\20_11_24\152430\152430_AverageTRPL_800_to_875nm.csv"
-tag3 = r'9-8-1-C'
-
-path4 = r"\\ad.ufl.edu\che\Hages-Lab\TRPL Data\Hages\Ferekides\20_11_24\180627\180627_AverageTRPL_800_to_875nm.csv"
-tag4 = r'9-8-1-D'
-
+path = r"\\ad.ufl.edu\che\Hages-Lab\TRPL Data\Hages\Ferekides\20_11_24\180627\180627_AverageTRPL_800_to_875nm.csv"
+tag = r'9-8-1-D'
 
 norm = True
 zero_shift = True
-
 
 def TRPL_Manipulate(path,tag,norm=False,zero_shift=False,t_shift=0):
 
@@ -46,25 +36,59 @@ def TRPL_Manipulate(path,tag,norm=False,zero_shift=False,t_shift=0):
     
     if t_shift != 0:
         TRPL[:,0] = TRPL[:,0] + t_shift
-        
-   
+       
     return TRPL, tag
 
-TRPL1, tag1 = TRPL_Manipulate(path1,tag1,norm,zero_shift,t_shift=0)
-TRPL2, tag2 = TRPL_Manipulate(path2,tag2,norm,zero_shift,t_shift=0)
-TRPL3, tag3 = TRPL_Manipulate(path3,tag3,norm,zero_shift,t_shift=0)
-TRPL4, tag4 = TRPL_Manipulate(path4,tag4,norm,zero_shift,t_shift=0)
+def Fit_1exp(TRPL_data,fitrange):
+   
+    def Exp1(time,A,tau):
+        return -time/tau + np.log(A)
     
+    #trim-data
+    low_index, high_index = (np.abs(TRPL_data[:,0]-np.min(fitrange))).argmin() , (np.abs(TRPL_data[:,0]-np.max(fitrange))).argmin()
+    TRPL_fit = TRPL_data[low_index:high_index,:]
+    
+    popt, pcov = curve_fit(Exp1,TRPL_fit[:,0],np.log(np.abs(TRPL_fit[:,1])))
+    perr = np.sqrt(np.diag(pcov))
+    
+    y_values = np.exp(Exp1(TRPL_fit[:,0],*popt))
+    output = np.vstack((TRPL_fit[:,0],y_values)).T
+    label =  r'$\tau:\ $' + np.array2string(popt[1], precision=2, separator=',', suppress_small=True) + ' ns'
+    
+    return output, label, popt, perr 
+
+def Fit_2exp(TRPL_data,fitrange):
+   
+    def Exp2(time,A1,A2,tau_1,tau_2):
+        return np.log(A1*np.exp(-time/tau_1) + A2*np.exp(-time/tau_2))
+    
+    #trim-data
+    low_index, high_index = (np.abs(TRPL_data[:,0]-np.min(fitrange))).argmin() , (np.abs(TRPL_data[:,0]-np.max(fitrange))).argmin()
+    TRPL_fit = TRPL_data[low_index:high_index,:]
+    
+    popt, pcov = curve_fit(Exp2,TRPL_fit[:,0],np.log(np.abs(TRPL_fit[:,1])))
+    perr = np.sqrt(np.diag(pcov))
+    
+    y_values = np.exp(Exp2(TRPL_fit[:,0],*popt))
+    output = np.vstack((TRPL_fit[:,0],y_values)).T
+    label =  r'$\tau_1:\ $' + np.array2string(np.min(popt[2:4]), precision=2, separator=',', suppress_small=True) + ' ns; ' + r'$\tau_2:\ $' + np.array2string(np.max(popt[2:4]), precision=2, separator=',', suppress_small=True) + ' ns'
+    
+    return output, label, popt, perr
+
+TRPL, tag = TRPL_Manipulate(path,tag,norm,zero_shift,t_shift=0)
+
+fit_range = [0,25]
+TRPL_fit, fit_label, popt, perr = Fit_2exp(TRPL,fit_range)
+
+
 plt.figure(0, dpi=120)
 plt.title(Plot_Title)
 plt.yscale('log')
 plt.xlabel(r'Delay / ns')
 plt.ylabel(r'Normalized Counts / a.u.')
-plt.ylim(1e-3,2)
-plt.xlim(-2, 40)
-plt.plot(TRPL1[:,0],TRPL1[:,1],label = tag1)
-plt.plot(TRPL2[:,0],TRPL2[:,1],label = tag2)
-plt.plot(TRPL3[:,0],TRPL3[:,1],label = tag3)
-plt.plot(TRPL4[:,0],TRPL4[:,1],label = tag4)
+plt.ylim(5e-5,2)
+plt.xlim(-1, 40)
+plt.plot(TRPL[:,0],TRPL[:,1],label = tag)
+plt.plot(TRPL_fit[:,0],TRPL_fit[:,1],'k--',label = fit_label)
 plt.legend()
 
