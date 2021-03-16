bb50b9da6349c42dffb570273f40cd2edd5a053f
charleshages <65269370+charleshages@users.noreply.github.com>
2021-03-16 18:29:06 -0400

    Standard Updates

--- interferogram_functions.py
+++ interferogram_functions.py
@@ -18,7 +18,7 @@ def import_INTR(path):
     allFiles = []
     b = glob.glob(path + "/*.txt")  # glob. lists the filename and path
     allFiles.extend(b)
-    
+
     for i in range(len(allFiles)):
         namestest = allFiles[i].split('_')[-1]
         if namestest == r"POS.txt":
@@ -31,9 +31,9 @@ def import_INTR(path):
                 intr_data = np.array(list(csv.reader(j,delimiter="\t")),dtype='float')[0,:]
     return pos_data, intr_data
 
-    
 
-def prep_interferogram(pos_data,intr_data,apodization_width,apod_type="BH",mean_sub="True",resample="True",resample_factor=2,shift="True",pad_test="True",padfactor=4,plots="True"):
+
+def prep_interferogram(pos_data,intr_data,apodization_width,apod_type="BH",mean_sub="True",resample="True",resample_factor=2,shift="True",pad_test="True",padfactor=4,plots="True",pltzoom=False):
     intr_data, pos_data = Interf_calibration1D.interf_calibration1D(intr_data,pos_data)
     shiftfactor = 0
     if mean_sub == "True":
@@ -44,7 +44,7 @@ def prep_interferogram(pos_data,intr_data,apodization_width,apod_type="BH",mean_
         intr_data = fcubic(pos_data)
     index_pos = np.argmin(abs(pos_data))
     if shift == "True":
-        index_intr = np.argmax(intr_data)  
+        index_intr = np.argmax(intr_data)
         shiftfactor=(pos_data[index_intr]-pos_data[index_pos])
         pos_data = pos_data-shiftfactor
     if apod_type != "None":
@@ -80,13 +80,13 @@ def prep_interferogram(pos_data,intr_data,apodization_width,apod_type="BH",mean_
         padlen = int(padfactor*(2**np.ceil(np.log(len(intr_data))/np.log(2))))-len(intr_data)
         intr_data = np.pad(intr_data,(0,int(padlen)),'constant', constant_values=(0))
         pos_data = np.append(pos_data,np.linspace(pos_data[-1]+posdiff,(pos_data[-1]+posdiff)+padlen*posdiff,num=padlen))
-        
+
     left_axis_intr, right_axis_intr = intr_data[0:index_pos+1], intr_data[index_pos+1:]
     left_axis_pos, right_axis_pos = pos_data[0:index_pos+1], pos_data[index_pos+1:]
-    preFFT_data, preFFT_pos = np.concatenate((right_axis_intr,left_axis_intr)), np.concatenate((right_axis_pos,left_axis_pos))   
-    
+    preFFT_data, preFFT_pos = np.concatenate((right_axis_intr,left_axis_intr)), np.concatenate((right_axis_pos,left_axis_pos))
+
     if plots == "True":
-        #Plot interferogram data    
+        #Plot interferogram data
         plt.figure(1, dpi=120)
         if apod_type != "None":
             plt.plot(raw_pos,raw_intr/np.max(raw_intr))
@@ -94,23 +94,26 @@ def prep_interferogram(pos_data,intr_data,apodization_width,apod_type="BH",mean_
             plt.plot(raw_pos,intr_func)
         else:
             plt.plot(pos_data,intr_data)
+        if pltzoom:
+            plt.xlim(-0.25,0.25)
         plt.xlabel('Position / mm')
         plt.ylabel('Counts / a.u.')
         plt.show()
-        
+
         plt.figure(2, dpi=120)
         plt.plot(preFFT_data)
         plt.show()
-        
+
     return preFFT_pos, preFFT_data, shiftfactor
 
 def FFT_intr(preFFT_pos,preFFT_data, plots="False",correct="True",scale="linear"):
+    # Treat single TS 1D case as 2D case
     if preFFT_data.ndim == 1:
         preFFT_data = preFFT_data.reshape((len(preFFT_data),1))
-    
+        
     #Do as much preparation outside of the loop over preFFT_data's timesteps as possible
     freq = np.fft.rfftfreq(preFFT_pos.shape[-1],np.diff(preFFT_pos)[0])
-        
+
     #Import calibration for WL
     items = os.listdir(".")
     for names in items:
@@ -125,9 +128,9 @@ def FFT_intr(preFFT_pos,preFFT_data, plots="False",correct="True",scale="linear"
     max_freq = np.max(reciprocal)
     # Is freq[] always in ascending order? If so this can be made much simpler with array slicing
     select = np.intersect1d(np.where(freq >= min_freq), np.where(freq <= max_freq))
-    
+
     fn = interp1d(reciprocal, wavelength, kind="linear")
-    
+
     # Do FFT on the data
     FFT_intr_full = np.fft.rfft(preFFT_data, axis=0)
 
@@ -142,7 +145,7 @@ def FFT_intr(preFFT_pos,preFFT_data, plots="False",correct="True",scale="linear"
     # Trim according to calibrations
     freq_trim = freq[select]
     FFT_intr_trim_full = FFT_final_full[select]
-    
+
     #Compute WL
     wave = fn(freq_trim)
     if plots == "True":
@@ -159,7 +162,7 @@ def FFT_intr(preFFT_pos,preFFT_data, plots="False",correct="True",scale="linear"
             plt.xlabel("Freq.")
             plt.legend()
             plt.show()
-    
+
             if correct == "True":
                 plt.figure(2, dpi=120)
                 plt.plot(freq,FFT_real,label="Real")
@@ -170,15 +173,15 @@ def FFT_intr(preFFT_pos,preFFT_data, plots="False",correct="True",scale="linear"
                 plt.xlabel("Freq.")
                 plt.legend()
                 plt.show()
-        
+
     return wave, FFT_intr_trim_full
 
 def import_MAP(path):
     allFiles = []
     b = glob.glob(path + "/*.txt")  # glob. lists the filename and path
     allFiles.extend(b)
-    
-    
+
+
     for i in range(len(allFiles)):
         namestest = allFiles[i].split('_')[-1]
         if namestest == r"POS.txt":
@@ -192,34 +195,33 @@ def import_MAP(path):
         elif namestest == r"TIME.txt":
             with open(allFiles[i],'r') as j:
                 time_data = np.array(list(csv.reader(j,delimiter="\t")),dtype='float')[0,:]
-    
+
     # Discard the zero columns
     standard=np.std(map_data, axis=0)
     indexes=np.asarray(np.nonzero(standard))
     map_data=map_data[:,indexes[0][0]:indexes[0][-1]]
     time_data =time_data[indexes[0][0]:indexes[0][-1]]/1e3
-    
+
     return pos_data, time_data, map_data
-    
+
 
 def prep_map(pos_data,map_data,apodization_width,apod_type="BH",mean_sub="True",resample="True",resample_factor=2,shift="True",pad_test="True",padfactor=4,plots="True"):
-    map_data, pos_data = Interf_calibration2D.interf_calibration2D(map_data,pos_data)   
+    map_data, pos_data = Interf_calibration2D.interf_calibration2D(map_data,pos_data)
     prep_build=[]
     pos_data_raw = pos_data
     
     if mean_sub == "True":
         map_data = map_data - np.mean(map_data, axis=0)
-        
+
     if resample == "True":
         fcubic = interp1d(pos_data_raw, map_data, kind='cubic', axis=0)
         pos_data_raw = np.linspace(pos_data_raw[0],pos_data_raw[-1],endpoint=True,num=len(pos_data_raw)*resample_factor)
         map_data = fcubic(pos_data_raw)
-    
+
     for i in range(map_data.shape[1]): # For each timestep
         intr_data = map_data[:,i] # Take a column
         pos_data = pos_data_raw
-        #pos_data2 = np.add.outer(pos_data_raw, np.zeros(map_data.shape[1]))
-        
+
         index_pos = np.argmin(abs(pos_data))
         if shift == "True":
             index_intr = np.argmax(intr_data)
@@ -232,8 +234,8 @@ def prep_map(pos_data,map_data,apodization_width,apod_type="BH",mean_sub="True",
             if apod_type == "Gauss":
                 intr_func = np.exp(-(2.24*(pos_data)/apodization_width)**2)
             if apod_type == "BH":
-                A0, A1, A2, A3 = 0.42323, 0.49755, 0.07922, 0.01168                
-                intr_func = np.where(abs(pos_data) <= apodization_width, 
+                A0, A1, A2, A3 = 0.42323, 0.49755, 0.07922, 0.01168
+                intr_func = np.where(abs(pos_data) <= apodization_width,
                                       A0+A1*np.cos(np.pi*pos_data/apodization_width)+A2*np.cos(2*np.pi*pos_data/apodization_width)+A3*np.cos(3*np.pi*pos_data/apodization_width),
                                       0)
             if apod_type == "Triangle":                        
@@ -249,11 +251,31 @@ def prep_map(pos_data,map_data,apodization_width,apod_type="BH",mean_sub="True",
             padlen = int(padfactor*(2**np.ceil(np.log(len(intr_data))/np.log(2))))-len(intr_data)
             intr_data = np.pad(intr_data,(0,int(padlen)),'constant', constant_values=(0))
             pos_data = np.append(pos_data,np.linspace(pos_data[-1]+posdiff,(pos_data[-1]+posdiff)+padlen*posdiff,num=padlen))
-        
-        index_pos = np.argmin(abs(pos_data))    
+
+        index_pos = np.argmin(abs(pos_data))
         left_axis_intr, right_axis_intr = intr_data[0:index_pos+1], intr_data[index_pos+1:]
         left_axis_pos, right_axis_pos = pos_data[0:index_pos+1], pos_data[index_pos+1:]
-        preFFT_data, preFFT_pos = np.concatenate((right_axis_intr,left_axis_intr)), np.concatenate((right_axis_pos,left_axis_pos))   
+        preFFT_data, preFFT_pos = np.concatenate((right_axis_intr,left_axis_intr)), np.concatenate((right_axis_pos,left_axis_pos))
         prep_build.append(preFFT_data)
-        
+
     return preFFT_pos, np.array(prep_build,dtype='float').T
+
+def fetch_metadata(dir_name,openfile):  #  "{}\\Param_Import_metadata.txt"  or "{}\\Plot_Params.txt"
+    with open(openfile.format(dir_name), "r") as ifstream:
+        param_values_dict = {}
+        for line in ifstream:
+            if "#" in line: continue
+
+            else:
+                param = line[0:line.find(':')]
+                new_value = line[line.find('\t') + 1:].strip('\n')
+
+                try:
+                    if "." in new_value:
+                        param_values_dict[param] = float(new_value)
+                    else:
+                        param_values_dict[param] = int(new_value)
+                except:
+                    param_values_dict[param] = str(new_value)
+
+    return param_values_dict
