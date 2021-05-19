7665c94307b826fa1ccc8e40a08cc6011573bab0
cfai2304 <cfai2304@gmail.com>
2021-05-19 15:18:25 -0400

    New INTR script

---
+++ Gemini_INTR_script_CJH.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 11:37:33 2020
+
+@author: Chuck
+"""
+
+from interferogram_functions import prep_interferogram,  FFT_intr, import_INTR
+import matplotlib.pyplot as plt
+from numpy import savetxt
+import os
+
+path = r"E:\GEMENI DAQ\NIREOS Complete Example V12_MCS_TimeHarp_32bit Folder\Measurement\20210519\150421"
+save_params = True          #Use this to create a txt file that can be imported into the "..._MAP_script"
+
+start_wave = 550           #For Plotting
+end_wave = 1100             #For Plotting
+pltzoomstate = False        #Zoom in around the zero position in interferogram to better observe oscillations
+pltzoomrange = [-.25,.25]   #Range to zoom in on if pltzoomstate=True
+
+apodization_width=0.5      #Bounds (negative to positive) outside of which the data = 0
+apod_type="BH"              #Function to use for apodization: "None" "Gauss" "Triangle" "Boxcar" or "BH" (Default)
+resample = True             #Enhance resolution by cubic interpolation
+resample_factor=4           #Factor to increase data points by
+shift= False                #Shift max value to be at 0 mm position - not sure it matters
+pad_test = True             #Pad the data with zeros to enhance FFT resolution
+padfactor = 14              #Total points will be filled with zeros untill 2**padfactor  -> 2**15 = 32k (default), 2**16=65k
+baseline_sub_state = False   #Perform IModPoly baseline subtraction if not a linear baseline (poly = 1)
+mean_sub = True             #Shift the average value of the interferogram to be zero
+
+pos_data, intr_data = import_INTR(path)
+
+preFFT_pos, preFFT_data, shiftfactor, baseline_fit = prep_interferogram(pos_data,intr_data,apodization_width,apod_type=apod_type,resample=resample,resample_factor=resample_factor,shift=shift,pad_test=pad_test,padfactor=padfactor,mean_sub=mean_sub,plots=True,pltzoom=pltzoomstate,zoom_range=pltzoomrange,baseline_sub_state=baseline_sub_state)
+wave, FFT_intr_trim = FFT_intr(preFFT_pos,preFFT_data,plots=True,scale="linear",correct=True)
+
+#Plot Full PL
+plt.figure(5, dpi=120)
+plt.ylabel("Counts / a.u.")
+plt.xlabel("Wavelength / nm")
+plt.title("Average PL")
+plt.plot(wave,FFT_intr_trim)
+plt.xlim(start_wave,end_wave)
+plt.yscale('linear')
+
+# Best to turn this on only when you have found the desired params
+if save_params:
+    outputfilename_meta = path + "\\" + os.path.split(path)[-1] + '_FFTmetadata.txt'
+    params = {"apod_width":apodization_width, "apod_type":apod_type, "do_resample":resample, "resample_factor":resample_factor,
+              "do_shift":shift, "do_padzeros":pad_test, "pad_factor":padfactor, "do_baseline_sub":baseline_sub_state, "do_mean_sub":mean_sub,"shift_factor":shiftfactor}
+    with open(outputfilename_meta.format(path), 'w+') as ofstream:
+        ofstream.write("# Params used in Gemini_INTR_script_CJH.py")
+        for param, val in params.items():
+            ofstream.write("\n{}:\t{}".format(param, val))
+
+    outputfilename_baseline = path + "\\" + os.path.split(path)[-1] + '_BaselineFit.txt'
+    savetxt(outputfilename_baseline,baseline_fit,delimiter='  ')
+
