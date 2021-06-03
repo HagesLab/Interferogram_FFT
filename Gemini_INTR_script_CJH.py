bc59c4a5c61acae2b3d7f561eb765a526631452b
charleshages <c.hages@ufl.edu>
2021-06-03 11:18:01 -0400

    Update Gemini_INTR_script_CJH.py
    Added the ability to use multiple apod widths to compare

--- Gemini_INTR_script_CJH.py
+++ Gemini_INTR_script_CJH.py
@@ -7,40 +7,50 @@ Created on Mon Nov 30 11:37:33 2020
 
 from interferogram_functions import prep_interferogram,  FFT_intr, import_INTR
 import matplotlib.pyplot as plt
-from numpy import savetxt
 import os
 
-path = r"E:\GEMENI DAQ\NIREOS Complete Example V12_MCS_TimeHarp_32bit Folder\Measurement\20210519\150421"
-save_params = True          #Use this to create a txt file that can be imported into the "..._MAP_script"
+path = r"R:\Hages-Lab\TRPL Data\Ruiquan\20210602\145304"
+save_params = True          #Use this to create a txt file that can be imported into the "..._MAP_script" and export Plots
 
-start_wave = 550           #For Plotting
+start_wave = 550            #For Plotting - keep in mind the LP filter value
 end_wave = 1100             #For Plotting
 pltzoomstate = False        #Zoom in around the zero position in interferogram to better observe oscillations
 pltzoomrange = [-.25,.25]   #Range to zoom in on if pltzoomstate=True
 
-apodization_width=0.5      #Bounds (negative to positive) outside of which the data = 0
+apodization_width=[0.7,0.5,0.3]     #Bounds (negative to positive) outside of which the data = 0, should be a list. Use many values in the list to compare Apod widths
 apod_type="BH"              #Function to use for apodization: "None" "Gauss" "Triangle" "Boxcar" or "BH" (Default)
 resample = True             #Enhance resolution by cubic interpolation
 resample_factor=4           #Factor to increase data points by
 shift= False                #Shift max value to be at 0 mm position - not sure it matters
 pad_test = True             #Pad the data with zeros to enhance FFT resolution
 padfactor = 14              #Total points will be filled with zeros untill 2**padfactor  -> 2**15 = 32k (default), 2**16=65k
-baseline_sub_state = False   #Perform IModPoly baseline subtraction if not a linear baseline (poly = 1)
+baseline_sub_state = False  #Perform IModPoly baseline subtraction if not a linear baseline (poly = 1) - doesn't work with MAP FFT yet
 mean_sub = True             #Shift the average value of the interferogram to be zero
+plots = True               #Deactivate plots from FFT and prep - useful if using more than one apod width to compare.
 
 pos_data, intr_data = import_INTR(path)
 
-preFFT_pos, preFFT_data, shiftfactor, baseline_fit = prep_interferogram(pos_data,intr_data,apodization_width,apod_type=apod_type,resample=resample,resample_factor=resample_factor,shift=shift,pad_test=pad_test,padfactor=padfactor,mean_sub=mean_sub,plots=True,pltzoom=pltzoomstate,zoom_range=pltzoomrange,baseline_sub_state=baseline_sub_state)
-wave, FFT_intr_trim = FFT_intr(preFFT_pos,preFFT_data,plots=True,scale="linear",correct=True)
+wave_list, FFT_intr_trim_list = [], []
+for i in range(len(apodization_width)):
+    preFFT_pos, preFFT_data, shiftfactor, baseline_fit = prep_interferogram(pos_data,intr_data,apodization_width[i],apod_type=apod_type,resample=resample,resample_factor=resample_factor,shift=shift,pad_test=pad_test,padfactor=padfactor,mean_sub=mean_sub,plots=plots,pltzoom=pltzoomstate,zoom_range=pltzoomrange,baseline_sub_state=baseline_sub_state)
+    wave, FFT_intr_trim = FFT_intr(preFFT_pos,preFFT_data,plots=True,scale="linear",correct=True)
+    wave_list.append(wave)
+    FFT_intr_trim_list.append(FFT_intr_trim)
 
 #Plot Full PL
 plt.figure(5, dpi=120)
 plt.ylabel("Counts / a.u.")
 plt.xlabel("Wavelength / nm")
 plt.title("Average PL")
-plt.plot(wave,FFT_intr_trim)
+for i in range(len(wave_list)):
+    plt.plot(wave_list[i],FFT_intr_trim_list[i],label=apod_type+' Apod '+str(apodization_width[i])+' mm')
 plt.xlim(start_wave,end_wave)
 plt.yscale('linear')
+if save_params:
+    PLname = path + "\\" + os.path.split(path)[-1] + '_PLPlot.png'
+    plt.savefig(PLname)
+if len(apodization_width) > 1:
+    plt.legend()
 
 # Best to turn this on only when you have found the desired params
 if save_params:
@@ -51,7 +61,3 @@ if save_params:
         ofstream.write("# Params used in Gemini_INTR_script_CJH.py")
         for param, val in params.items():
             ofstream.write("\n{}:\t{}".format(param, val))
-
-    outputfilename_baseline = path + "\\" + os.path.split(path)[-1] + '_BaselineFit.txt'
-    savetxt(outputfilename_baseline,baseline_fit,delimiter='  ')
-
